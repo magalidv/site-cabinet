f7e87c65ecf074162a2538fc4d04b991f8156583
magalidv <magali.daveluy@gmail.com>
2026-06-12 16:32:07 +0200

    changing favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,15 +13,10 @@ export const metadata = {
     icon: [
       { url: "/favicon.ico" },
       {
-        url: "/favicon-16x16.png",
-        sizes: "16x16",
+        url: "/favicon-96x96.png",
+        sizes: "96x96",
         type: "image/png",
-      },
-      {
-        url: "/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
+      }
     ],
     apple: "/apple-touch-icon.png",
   },
